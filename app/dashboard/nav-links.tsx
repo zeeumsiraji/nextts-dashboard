bbf4b6e223eed73e51eeea3ff796c35428c420a2
zeeumsiraji <zeeumsiraji@gmail.com>
2026-03-19 01:59:50 +0600

    solve these eror

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -8,15 +8,31 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
- 
-// ...
+
+const links = [
+  {
+    name: 'Home',
+    href: '/dashboard',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  {
+    name: 'Customers',
+    href: '/dashboard/customers',
+    icon: UserGroupIcon,
+  },
+];
  
 export default function NavLinks() {
   const pathname = usePathname();
  
   return (
     <>
-      {links.map((link) => {
+      {links.map((link: typeof links[0]) => {
         const LinkIcon = link.icon;
         return (
           <Link
