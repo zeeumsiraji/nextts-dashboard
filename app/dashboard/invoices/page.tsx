d2fbe28eed3a53d701d40d0a45d84cfe70ddc950
zeeumsiraji <zeeumsiraji@gmail.com>
2026-03-26 20:39:36 +0600

    Complite this Project

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,12 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
  
+
+ export const metadata: Metadata = {
+  title: 'Invoices',
+};
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
